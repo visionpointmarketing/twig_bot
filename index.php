32fef6ec5d52bff3bd95344221d7affb85fa5bdd
Chris Francart <root@vpm.puphpet>
2016-08-18 13:38:41 -0500

    cleanup for presentation

--- index.php
+++ index.php
@@ -1,65 +1,61 @@
 <?php
-include_once($_SERVER['DOCUMENT_ROOT'].'/includes/internals/loader.twig');
+include_once($_SERVER['DOCUMENT_ROOT'].'/includes/internals/loader.php');
 
-$header = 'h_generic';
-$template = '2col';
-$footer = 'f_generic';
+$pagetemplate = '2col';
 
-echo $twig -> render('head.tmpl');
-echo $twig -> render('@headers/'.$header.'.html',
-	array(
-		'navigation'=> array(
-			'val' =>main_nav(),
-			'class'=>'navigation n-header'
-		)
+echo $twig -> render('head.twig');
+echo $twig -> render($header,[
+	'navigation'=> array(
+		'val' =>main_nav(),
+		'class'=>'navigation n-header'
 	)
-);
-echo $twig->render('@templates/'.$template.'.twig',
-	[
-		'content'=> [
-			'header'=>[
-				'hval'=>'2',
-				'title'=>'sample'
-			],
-			'list',
-			'button'=>[
-				'btnclass'=>'dark'
-			],
-			'button',
-			'list'
+]);
+echo $twig->render('@templates/'.$pagetemplate.'.twig',[
+	'content'=> [
+		'header'=>[
+			'hval'=>'2',
+			'title'=>'sample'
 		],
-		'promo_sidebar'=> [
-			'header'=>[
-				'hval'=>'3',
-				'title'=>'sample'
-			],
-			'button'=>[
-				'btnclass'=>'dark'
-			],
-			'button'
-		]
+		'list',
+		'button'=>[
+			'btnclass'=>'dark'
+		],
+		'button',
+		'list',
+		'markup'=>[
+			'val'=>'
+				<div class="custom">
+					<p>some <span style="color:blue;">custom</span> 
+					<span style="background:#ccc;">HTML</span> markup</p>
+				</div>
+			',
+		],
+		'function',
+	],
+	'promo_sidebar'=> [
+		'header'=>[
+			'hval'=>'3',
+			'title'=>'sample'
+		],
+		'button'=>[
+			'btnclass'=>'dark'
+		],
+		'button'
+	],
+	'this_region_doesnt_exist'=>[
+		'button'
 	]
-);
-echo $twig->render('@footers/'.$footer.'.html',
-	array(
-		'footermenu'=> array(
-			'val' =>array(
-				'<a href="#">About Us</a>',
-				'<a href="#">Admissions</a>',
-				'<a href="#">Academics</a>',
-				'<a href="#">Student Life</a>',
-				'<a href="#">News & Events</a>'
-			),
-			'class'=>'navigation n-footer'
-		)
-	)
-);
-
-switch($uri){
-	case '/':
-		//echo $twig->render('@templates/'.$template.'.html');
-		break;
-	default:
-		//echo $twig->render('@templates/'.$template.'.html');
-}
+]);
+echo $twig->render($footer,[
+	'footermenu'=> [
+		'val' =>[
+			'<a href="#">About Us</a>',
+			'<a href="#">Admissions</a>',
+			'<a href="#">Academics</a>',
+			'<a href="#">Student Life</a>',
+			'<a href="#">News & Events</a>'
+		],
+		'class'=>'navigation n-footer'
+	]
+]);
 ?>
